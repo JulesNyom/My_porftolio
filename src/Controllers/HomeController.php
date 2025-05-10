523b3b133525994490b2c71c32f9903f52228dfa
JulesNyom <julesnyom@gmail.com>
2025-05-10 14:13:45 +0200

    Finished the first controller.

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -1,6 +1,18 @@
 <?php
 class Homecontroller{
     public function index () {
-        
+        $experiences = [
+            [
+            'titre' => 'Développeur web et mobile',
+            'Organisation' => 'indépendant',
+            'période' => '2024 à maintenant',
+            'Description' => 'Je conçois et développe des applications pour mon propre compte.',
+            'skills' => 'PHP, Laravel, React, Git, Java, Sprint boot, React native.'
+            ]
+        ];
+        require __DIR__ . '/../Views/home.php';
     }
 }
+
+$controller = new Homecontroller();
+$controller->index();
